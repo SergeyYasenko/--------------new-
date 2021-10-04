5a30f47778509dce04987c5fe440318e39ce3030
SergeyYasenko <91138847+SergeyYasenko@users.noreply.github.com>
2021-10-04 14:38:15 +0300

    new

--- js/main.js
+++ js/main.js
@@ -194,23 +194,23 @@ document.addEventListener('DOMContentLoaded', function () {
    }
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+   //E-mail Ajax Send
+   $("form").submit(function () { //Change
+      var th = $(this);
+      $.ajax({
+         type: "POST",
+         url: "mail.php", //Change
+         data: th.serialize()
+      }).done(function () {
+         alert("Thank you!");
+         setTimeout(function () {
+            // Done Functions
+            th.trigger("reset");
+         }, 1000);
+      });
+      return false;
+   });
 
 });
